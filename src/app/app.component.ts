6b85bfc7049a9f04c2e18de7c416108f558f2b20
Jude Kuti <judekuti@gmail.com>
2018-03-21 07:32:46 -0500

    Create increment function along side counter variable set to 0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Redux with Ng';
+  counter = 0;
+  increment(){
+      this.counter++;
+  }
 }
